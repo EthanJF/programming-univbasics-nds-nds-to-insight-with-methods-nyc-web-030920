4a9030045a4c4737016c29af5bf3e10d83e1638c
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-04 20:14:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,20 +4,18 @@ require 'pp'
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-# def gross_for_director(director_data)
-#   i = 0
-#   total = 0
-#   while i < director_data.length do
-#     total += director_data[[i]
-# end
+def gross_for_director(director_data)
+  i = 0
+  total = 0
+  while i < director_data.length do
+    total += director_data[[i]
+end
 
-# # Write a method that, given an NDS creates a new Hash
-# # The return value should be like:
-# #
-# # { directorOne => allTheMoneyTheyMade, ... }
-# def directors_totals(nds)
-#   result = {}
-#   result
-# end
-
-puts pp directors_database
+# Write a method that, given an NDS creates a new Hash
+# The return value should be like:
+#
+# { directorOne => allTheMoneyTheyMade, ... }
+def directors_totals(nds)
+  result = {}
+  result
+end
